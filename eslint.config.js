1a521f4df658c398aff23093512d54719a0c5cec
jhipster <jhipster@159b21949826>
2025-03-21 21:33:12 +0000

    Apply module: typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import typescript from 'typescript-eslint';
-import cypress from 'eslint-plugin-cypress/flat';
 
 export default typescript.config(
   {
@@ -15,18 +14,6 @@ export default typescript.config(
     ignores: ['target/'],
   },
   js.configs.recommended,
-  {
-    files: ['src/test/webapp/e2e/**/*.ts'],
-    extends: [...typescript.configs.recommendedTypeChecked, cypress.configs.recommended],
-    languageOptions: {
-      parserOptions: {
-        project: ['src/test/webapp/e2e/tsconfig.json'],
-      },
-    },
-    rules: {
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-    },
-  },
   ...typescript.configs.recommended.map(config => (config.name === 'typescript-eslint/base' ? config : { ...config, files: ['**/*.ts'] })),
   {
     files: ['src/*/webapp/**/*.ts'],
